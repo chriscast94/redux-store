789e2a2c9c8f99efc634290710c8ec6159261a9b
Christian Castillo <chriscast94@outlook.com>
2022-01-30 16:49:04 -0800

    removed context from models for redux

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import { useStoreContext } from "../../utils/GlobalState";
 import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
 
 const CartItem = ({ item }) => {
-
-  const [, dispatch] = useStoreContext();
 
   const removeFromCart = item => {
     dispatch({
--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
 import ProductItem from '../ProductItem';
-import { useStoreContext } from '../../utils/GlobalState';
 import { UPDATE_PRODUCTS } from '../../utils/actions';
 import { useQuery } from '@apollo/client';
 import { QUERY_PRODUCTS } from '../../utils/queries';
@@ -8,7 +7,6 @@ import { idbPromise } from '../../utils/helpers';
 import spinner from '../../assets/spinner.gif';
 
 function ProductList() {
-  const [state, dispatch] = useStoreContext();
 
   const { currentCategory } = state;
 
